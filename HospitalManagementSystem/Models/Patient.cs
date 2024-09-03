bde78878af0167f9670a8f08c3af864a83281b87
idgaf2133 <155026436+idgaf2133@users.noreply.github.com>
2024-09-03 14:57:07 +1000

    installed Microsoft.EntityFrameworkCore and created the first entity patient

---
+++ HospitalManagementSystem/Models/Patient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace HospitalManagementSystem.Models
+{
+    public class Patient
+    {
+
+        [Key]
+
+        public int patientID;
+        public string name;
+        public DateTime DOB;
+        public string gender;
+        public string contact;
+        public string address;
+        public string medicalHistory;
+
+
+
+
+    }
+}
